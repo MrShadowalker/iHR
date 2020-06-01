02cf4755084c12995c9a2575a1736642d486ed11
MrShadowalker <mr.shadowalker@foxmail.com>
2020-06-01 19:29:13 +0800

    代码格式调整+简单配置+文档

---
+++ test/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author Shadowalker
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("两件\t");
+        System.out.print("三四件\t");
+        String s = "abc\t";
+        System.out.print(s.length());
+    }
+}
--- vhr/vhrserver/vhr-mapper/src/main/java/org/javaboy/vhr/mapper/AdjustSalaryMapper.java
+++ vhr/vhrserver/vhr-mapper/src/main/java/org/javaboy/vhr/mapper/AdjustSalaryMapper.java
@@ -2,11 +2,26 @@ package org.javaboy.vhr.mapper;
 
 import org.javaboy.vhr.model.AdjustSalary;
 
+/**
+ * @author Shadowalker
+ */
 public interface AdjustSalaryMapper {
+    /**
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Integer id);
 
+    /**
+     * @param record
+     * @return
+     */
     int insert(AdjustSalary record);
 
+    /**
+     * @param record
+     * @return
+     */
     int insertSelective(AdjustSalary record);
 
     AdjustSalary selectByPrimaryKey(Integer id);
